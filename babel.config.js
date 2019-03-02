977582152f80d42e427dc7e1f6c919171bd20cb2
Giovanni Orlando <gorlandor@gmail.com>
2019-03-02 02:34:28 -0400

    Add basic server side rendering

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,11 @@
 module.exports = {
     presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: ['@babel/plugin-proposal-class-properties'],
+    plugins: [
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-transform-react-jsx',
+        '@babel/plugin-transform-runtime',
+        '@babel/plugin-proposal-async-generator-functions',
+        '@babel/plugin-transform-async-to-generator',
+        '@babel/plugin-transform-regenerator',
+    ],
 };
--- server/server.js
+++ server/server.js
@@ -1,13 +1,47 @@
-const express = require('express');
+import express from 'express';
+import path from 'path';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import App from '../client/src/components/App';
 const app = express();
-const path = require('path');
 
 app.set('port', process.env.PORT || 5000);
 
-app.get('/', function(req, res) {
-    res.send('hello world, jolly pokedex');
+app.use('/static', express.static(path.resolve(__dirname, '../client/public')));
+
+app.get('/*', function(req, res) {
+    const jsx = <App />;
+    const reactDom = renderToString(jsx);
+
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+    });
+
+    res.end(htmlTemplate(reactDom));
 });
 
 app.listen(app.get('port'), () => {
     console.log('Node app is running on http://localhost:'.concat(app.get('port')));
 });
+
+function htmlTemplate(reactDom) {
+    return `
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <title>Jolly Pokedex</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                
+                <link rel="stylesheet" href="./static/styles.bundle.css">
+                <script src="./static/vendors.bundle.js"></script>
+            </head>
+
+            <body>
+                <div id="jolly-pokedex">${reactDom}</div>
+                <script src="./static/client.bundle.js"></script>
+            </body>
+        </html>
+    `;
+}
