e335d1d875e72a2010d341c96d2037a1dd472e1f
Giovanni Orlando <gorlandor@gmail.com>
2019-03-02 01:27:05 -0400

    Adds react and sets up webpack.

---
+++ client/src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+    return <header>Hello, world!</header>;
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        'styles.bundle': './client/src/styles.js',
+        'client.bundle': './client/src/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'client/public'),
+        filename: '[name].js',
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(svg|png|jpg)$/,
+                use: 'url-loader',
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: [
+                            '@babel/plugin-proposal-async-generator-functions',
+                            '@babel/plugin-transform-async-to-generator',
+                            '@babel/plugin-transform-regenerator',
+                            '@babel/plugin-transform-runtime',
+                        ],
+                    },
+                },
+            },
+        ],
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors.bundle',
+                    chunks: 'all',
+                },
+            },
+        },
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+            },
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+    ],
+};
